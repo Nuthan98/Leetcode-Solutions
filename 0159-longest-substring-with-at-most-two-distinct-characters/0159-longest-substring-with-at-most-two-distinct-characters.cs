c7f422f9b2a1a9308d273fe6b06cedc4c75ee779
Nuthan98 <63735903+Nuthan98@users.noreply.github.com>
2026-09-13 18:04:11 +0530

    Time: 41 ms (63.16%), Space: 47.6 MB (31.58%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
@@ -4,30 +4,29 @@ public class Solution {
         int l = 0;
         int r = 0;
         Dictionary<char, int> res = new Dictionary<char, int>();
-        char[] charS = s.ToCharArray();
 
-        while(r<charS.Length)
+        while(r<s.Length)
         {
-            if(res.ContainsKey(charS[r]))
+            if(res.ContainsKey(s[r]))
             {
-                int count = res[charS[r]];
+                int count = res[s[r]];
                 count++;
-                res[charS[r]] = count;
+                res[s[r]] = count;
             }
             else
             {
-                res[charS[r]] = 1;
+                res[s[r]] = 1;
             }
             r++;
 
             while(res.Count > 2)
             {
-                int count = res[charS[l]];
+                int count = res[s[l]];
                 count--;
-                res[charS[l]] = count;
+                res[s[l]] = count;
 
-                if(res[charS[l]]==0)
-                    res.Remove(charS[l]);
+                if(res[s[l]]==0)
+                    res.Remove(s[l]);
 
                 l++;
             }
